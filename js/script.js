f848a14019eb8664c59f21cd9a473d38623f8fc2
Fahmeda Hasan Fariha <58398404+fahmedahasanfariha@users.noreply.github.com>
2021-05-02 18:04:10 +0600

    feature

--- js/script.js
+++ js/script.js
@@ -2,11 +2,34 @@ $(".banner-slider").slick({
     arrows: false,
     dots: true,
     fade: true,
-})
+});
 $(".image-slider").slick({
     arrows: false,
     dots: true,
     slidesToShow: 5,
-})
+});
 
 $('.venobox').venobox(); 
+
+
+// FEATURE PART
+
+
+$(".item").click(function(){
+    $(".item-content").html("d-block");
+    $(".item-content").show()
+
+});
+// $(".show-2").click(function(){
+//     $(".item-content-1").hide()
+//     $(".item-content-2").show()
+// });
+// $(".show-3").click(function(){
+//     $(".item-content-3").fadeToggle()
+// });
+// $(".show-4").click(function(){
+//     $(".item-content-4").fadeToggle()
+// });
+// $(".show-5").click(function(){
+//     $(".item-content-5").fadeToggle()
+// });
