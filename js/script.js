a9740a85604624784617d03e720dc2bbeac20a31
Fahmeda Hasan Fariha <58398404+fahmedahasanfariha@users.noreply.github.com>
2021-05-03 12:11:36 +0600

    up to price part

--- js/script.js
+++ js/script.js
@@ -1,35 +1,31 @@
+// ************************BANNER-SLIDER************************
 $(".banner-slider").slick({
     arrows: false,
     dots: true,
     fade: true,
 });
+// ************************IMAGE-SLIDER************************
 $(".image-slider").slick({
     arrows: false,
     dots: true,
     slidesToShow: 5,
 });
-
+// ************************VENOBOX************************
 $('.venobox').venobox(); 
 
+// ************************FEATURE-PART:MixItUp************************
+$(document).ready(function() {
 
-// FEATURE PART
-
-
-$(".item").click(function(){
-    $(".item-content").html("d-block");
-    $(".item-content").show()
-
+    var containerEl = document.querySelector('.mixitup');
+    var mixer = mixitup(containerEl, {
+        
+        animation: {
+            duration: 10
+        },
+        
+    });
+    mixer.filter('.item-content-1');
+    
 });
-// $(".show-2").click(function(){
-//     $(".item-content-1").hide()
-//     $(".item-content-2").show()
-// });
-// $(".show-3").click(function(){
-//     $(".item-content-3").fadeToggle()
-// });
-// $(".show-4").click(function(){
-//     $(".item-content-4").fadeToggle()
-// });
-// $(".show-5").click(function(){
-//     $(".item-content-5").fadeToggle()
-// });
+   
+
